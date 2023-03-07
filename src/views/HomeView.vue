d03729c12f20f1b1cce4a53b463da771377956b6
cedrick <jctdelacarcel@gmail.com>
2023-03-07 14:56:44 +0800

    remove and  added files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <script setup>
-
+import Navigation from '@/components/Navigation.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
 </script>
 
 <template>
-    <h1>Hello World</h1>
+    <Navigation/>
+    <main class="bg-red-500">
+        <section class="">
+            <h1>Introduction</h1>
+        </section>
+    </main>
+    <FooterComponent/>
 </template>
